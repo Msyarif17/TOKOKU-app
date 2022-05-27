aea52a65089f7547224a1610a5d13b825c5e9a69
NaughtyRhino <m.syarifsetiadi@gmail.com>
2022-05-27 20:59:25 +0700

    test

---
+++ database/factories/SupplyerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplyer>
+ */
+class SupplyerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nama'=>$this->faker->name(),
+            'alamat'=>$this->faker->address,
+            'nomor_telepon' => random_int(100000000000, 999999999999)
+        ];
+    }
+}
