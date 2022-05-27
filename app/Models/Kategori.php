df74bfe078963e55eacca67da71d77f3fc07540f
NaughtyRhino <m.syarifsetiadi@gmail.com>
2022-05-28 01:45:09 +0700

    test

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\Barang;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kategori extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $table ="kategori_barang";
     protected $fillable = ['nama','kode'];
 
